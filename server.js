ef87ba36b2497bbfe137492686b6791cb1848adb
anishazahan <anishazahan13@gmail.com>
2023-02-17 23:19:18 +0600

    initially first server running code added

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const app = require("./app");
+const dotenv = require('dotenv');
+
+// config 
+
+dotenv.config({path:"server/config/config.env"})
 
 
-
+app.get("/user",(req,res)=>{
+    res.send("user found")
+})
 
 app.listen(process.env.PORT,()=>{
     console.log("server is running")
