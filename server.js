f41f2d1a027f4b32c403fa93b5549ca44ad33c6a
anishazahan <anishazahan13@gmail.com>
2023-02-19 20:43:36 +0600

    Handling Uncaught Exception

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDatabase = require('./config/database')
 
+// Handling Uncaught Exception
+ process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+  });
+
+
 app.use(cors())
 
 // config 
@@ -16,9 +24,21 @@ connectDatabase();
 const PORT =process.env.PORT || 4000;
 
 
-// const port = process.env;
-// console.log(port.PORT);
+//--------server running code 
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log(`server is running http://localhost:${PORT}`)
 })
+
+
+
+// Unhandled Promise Rejection
+
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close(() => {
+      process.exit(1);
+    });
+  });
