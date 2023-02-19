848d322463277837f200858d43a06034e46fffeb
anishazahan <anishazahan13@gmail.com>
2023-02-19 15:31:35 +0600

    succesfully sample update product

--- controller/productController.js
+++ controller/productController.js
@@ -17,12 +17,47 @@ exports.createProduct = async (req,res,next)=>{
 }
 
 
+
+// get Product -- for admin
+
 exports.getAllProducts= async(req,res)=>{
     try {
         const product = await Product.find()
-    res.status(200).json(product);
+    res.status(200).json({
+        success: true,
+        product
+    });
     } catch (error) {
         res.status(500).send(error.message);
     }
+
+}
+
+
+// update Product -- for admin
+
+
+exports.updateProducts= async(req,res,next)=>{
+    let product =await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message:"Product Not Found."
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+      });
+
+      res.status(200).json({
+        success: true,
+        product,
+      });
+
+      next();
 
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllProducts, createProduct } = require('../controller/productController');
+const { getAllProducts, createProduct, updateProducts } = require('../controller/productController');
 const router = express.Router();
 
 
 router.route("/products").get(getAllProducts);
-router.route("/products/newProduct").post(createProduct);
+router.route("/product/newProduct").post(createProduct);
+router.route("/product/:id").put(updateProducts);
 
 
 
