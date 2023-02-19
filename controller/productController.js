96fd7a5a79de732b7f88c31f2496453c1668ebcc
anishazahan <anishazahan13@gmail.com>
2023-02-19 15:12:30 +0600

    succefully sample data post in mongodb

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,28 @@
+const Product = require("../models/productModel")
 
 
-exports.getAllProducts=(req,res)=>{
-    res.status(200).json({message:"product found"})
+
+// Create Product -- for admin
+
+exports.createProduct = async (req,res,next)=>{
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product,
+      });
+
+      next();
+}
+
+
+exports.getAllProducts= async(req,res)=>{
+    try {
+        const product = await Product.find()
+    res.status(200).json(product);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 
 }
